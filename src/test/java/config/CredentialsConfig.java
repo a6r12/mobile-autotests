60162dc9c680d8cc3b673c34ebc5b1d8d78175cf
akida <96776061+a6r12@users.noreply.github.com>
2022-04-23 00:37:46 +0300

    added android wiki test

---
+++ src/test/java/config/CredentialsConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:credentials.properties"
+})
+public interface CredentialsConfig extends Config {
+
+    @Key("user")
+    String user();
+
+    @Key("password")
+    String password();
+
+    @Key("app")
+    String app();
+
+    @Key("url")
+    String url();
+}
