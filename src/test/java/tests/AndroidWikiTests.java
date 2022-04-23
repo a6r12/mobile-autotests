c4ba81607830d781ae708801f514f25d3b3cd692
akida <96776061+a6r12@users.noreply.github.com>
2022-04-23 20:54:37 +0300

    added android wiki test tag

--- src/test/java/tests/AndroidWikiTests.java
+++ src/test/java/tests/AndroidWikiTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -11,6 +12,7 @@ import static io.qameta.allure.Allure.step;
 
 public class AndroidWikiTests extends TestBase {
 
+    @Tag("selenide")
     @Test
     void searchTest() {
 
