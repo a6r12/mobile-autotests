e2a7d3d87a9d8aa38db105de896888f7b4591877
akida <96776061+a6r12@users.noreply.github.com>
2022-04-23 21:27:05 +0300

    added android wiki test fix

--- src/test/java/tests/AndroidWikiTests.java
+++ src/test/java/tests/AndroidWikiTests.java
@@ -16,8 +16,8 @@ public class AndroidWikiTests extends TestBase {
     @Test
     void searchTest() {
 
-        step("Skip Language Selection", () ->
-                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click());
+//        step("Skip Language Selection", () ->
+//                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click());
         step("Type search", () -> {
             $(MobileBy.AccessibilityId("Search Wikipedia")).click();
             $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("gulag");
