1063be09535393d427dea5e80a49b56ca65e9ab3
akida <96776061+a6r12@users.noreply.github.com>
2022-05-09 15:15:18 +0300

    add new test

--- src/test/java/tests/AndroidWikiTests.java
+++ src/test/java/tests/AndroidWikiTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -27,4 +28,11 @@ public class AndroidWikiTests extends TestBase {
 
     }
 
+    @Test
+    void verifyNewsTest() {
+        step("Verify news page", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/view_card_header_title"))
+                    .shouldHave(Condition.text("In the news"));
+        });
+    }
 }
